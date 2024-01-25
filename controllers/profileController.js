2168c0c933e3ac08005bb88dcf7eac5aae4f6607
DELL <tirthshsh2123@gmail.com>
2024-01-25 12:01:48 +0530

    first commit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -30,34 +30,12 @@ exports.getProfiles = async (req, res) => {
   res.send(profilesWithRatings);
 };
 
-const multer = require('multer');
-const Profile = require('../models/profileModel');
-const Review = require('../models/reviewModel');
-
-exports.profileRegister = async (req, res) => {
-  const { serviceProviderId, phoneNo, companyName, service, whatsappNumber, experience, address } = req.body; 
-  const images = req.files.map(file => file.path); 
-  
-  const existingProfile = await Profile.findOne({ whatsappNumber: whatsappNumber });
-  if (existingProfile) {
-    return res.status(409).send('Profile with this phone number already exists');
+exports.getProfile = async (req, res) => {
+  const phoneNo = req.params.phoneNo;
+  const profile = await Profile.findOne({ phoneNo: phoneNo });
+  if (profile) {
+      res.json(profile);
+  } else {
+      res.status(404).send('Profile not found');
   }
-
-  const profile = new Profile({ serviceProviderId, phoneNo, companyName, service, whatsappNumber, experience, address, images }); 
-  await profile.save();
-  res.send('Profile registered successfully');
-};
-
-exports.getProfiles = async (req, res) => {
-  const profiles = await Profile.find({});
-  
-  const profilesWithRatings = await Promise.all(profiles.map(async (profile) => {
-    const reviews = await Review.find({ serviceProviderContact: profile.phoneNo });
-    const ratings = reviews.map(review => review.rating);
-    const totalRatings = ratings.length;
-    const averageRating = totalRatings > 0 ? ratings.reduce((a, b) => a + b, 0) / totalRatings : 0;
-    return { ...profile._doc, totalRatings, averageRating };
-  }));
-
-  res.send(profilesWithRatings);
 };
